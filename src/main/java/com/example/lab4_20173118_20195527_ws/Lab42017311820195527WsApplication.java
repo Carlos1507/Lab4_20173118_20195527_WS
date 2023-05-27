c21fff77e12fbda58c92b9e0f18285badfd49577
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-05-27 12:53:13 -0500

    inicio

---
+++ src/main/java/com/example/lab4_20173118_20195527_ws/Lab42017311820195527WsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20173118_20195527_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab42017311820195527WsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab42017311820195527WsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_20173118_20195527_ws/Lab42017311820195527WsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20173118_20195527_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab42017311820195527WsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
